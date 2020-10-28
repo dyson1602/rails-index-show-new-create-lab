66bf6756abb863ae660afdacb68e733d365057dd
dyson1602 <cclark000@citymail.cuny.edu>
2020-10-27 20:25:40 -0400

    these tests don't pass, and all my code matches the solution completely at this point

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,11 @@
 Rails.application.routes.draw do
+
+  get 'coupons/index'
+
+  resources :coupons, only: [:index, :show, :new, :create]
+
+  # get '/coupons/:coupon_code', to: 'coupons#show', as: "coupon"
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
